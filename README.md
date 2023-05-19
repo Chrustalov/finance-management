2fcc891fe16b55078033545ffb20b60cc5680d14
Uppercode <oleksiwe22@gmail.com>
2023-05-19 17:54:27 +0300

    fix pagination

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version
+* 3.1.0
 
-* System dependencies
+* sqlite3
 
 * Configuration
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,15 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_home
   def index
-    
-    @pagy1, @credits = pagy(Credit.where(user_id: current_user.id), items: 8)
-    @pagy2, @deposits = pagy(Deposit.where(user_id: current_user.id), items: 8)
-    @pagy3, @outlays = pagy(Outlay.where(user_id: current_user.id), items: 8)
-    @pagy4, @incomes = pagy(Income.where(user_id: current_user.id), items: 8)
+    @credits = Credit.where(user_id: current_user.id)
+    @deposits = Deposit.where(user_id: current_user.id)
+    @outlays = Outlay.where(user_id: current_user.id)
+    @incomes = Income.where(user_id: current_user.id)
+
+    @pagy1, @incomes = pagy(@incomes, items: 8)
+    @pagy2, @outlays = pagy(@outlays, items: 8)
+    @pagy3, @deposits = pagy(@deposits, items: 8)
+    @pagy4, @credits = pagy(@credits, items: 8)
   end
 
   private
